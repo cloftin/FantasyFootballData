ce8b0ee9771150410686cc3d095ef9e0231787a8
ctloftin <ctloftin@ncsu.edu>
2018-08-09 17:05:36 -0400

    Added ability to get_yahoo_rankings without updating the data

--- R/get_yahoo_rankings.R
+++ R/get_yahoo_rankings.R
@@ -1,68 +1,46 @@
 
 #' @export
-get_yahoo_rankings <- function() {
+get_yahoo_rankings <- function(update = FALSE) {
   options(stringsAsFactors = FALSE)
 
-  base.url = "https://partners.fantasypros.com/external/widget/nfl-staff-rankings.php?source=2&id=7:8:9:285:699&year=2018&week=0&position=ALL&scoring=HALF&ajax=true&width=640&export=xls"
+  ranks <- read.csv(file = "/Users/colin/Documents/GitHub/FantasyFootballData/data/yahoo_ranks.csv", header = T, stringsAsFactors = F)
+  ranks <- ranks[order(ranks[,ncol(ranks)]),]
+  colnames(ranks) <- gsub("X", "", colnames(ranks))
+  colnames(ranks) <- gsub("\\.", "/", colnames(ranks))
 
-  yahoorankings <- readLines(base.url)
+  if(update) {
+    YahooFantasyAPI::check_token()
+    # YahooFantasyAPI::get_token(readLines("yahoocreds.txt")[1], readLines("yahoocreds.txt")[2])
 
-  yahoorankings <- yahoorankings[c(5:length(yahoorankings))]
-  yahoorankings <- plyr::ldply(yahoorankings, function(x) {
-    unlist(strsplit(x, "\\t"))[c(1:4)]
-  })
-  yahoorankings <- yahoorankings %>% select(V2, V1, V3, V4)
-  yahoorankings$V1 <- as.numeric(yahoorankings$V1)
-  colnames(yahoorankings) <- c("Player", format(Sys.Date(), "%m/%d"), "PosRank", "Team")
-  yahoorankings$PosRank <- gsub("RB",  "RB_",   yahoorankings$PosRank)
-  yahoorankings$PosRank <- gsub("WR",  "WR_",   yahoorankings$PosRank)
-  yahoorankings$PosRank <- gsub("QB",  "QB_",   yahoorankings$PosRank)
-  yahoorankings$PosRank <- gsub("TE",  "TE_",   yahoorankings$PosRank)
-  yahoorankings$PosRank <- gsub("K",   "K_",    yahoorankings$PosRank)
-  yahoorankings$PosRank <- gsub("DST", "DST_", yahoorankings$PosRank)
-  yahoorankings$Pos <- unlist(lapply(yahoorankings$PosRank, function(x) {
-    strsplit(x, "_")[[1]][1]
-  }))
+    yahoorankings <- YahooFantasyAPI::get_player_list(gameid = "380", leagueid = "121038", numPlayers = 300)
+    yahoorankings$name <- gsub(" II", "", yahoorankings$name)
+    yahoorankings$name <- gsub(" Jr.| Sr.", "", yahoorankings$name)
+    yahoorankings$name <- gsub(" V", "", yahoorankings$name)
+    yahoorankings$team <- gsub("JAX", "JAC", yahoorankings$team)
+    yahoorankings <- yahoorankings[-grep("DeMarco Murray", yahoorankings$name),]
 
-  yahoorankings <- yahoorankings %>% filter(Pos != "DST", Pos != "K")
-  yahoorankings <- yahoorankings %>% select(-PosRank, -Pos)
+    # yahoorankings <- yahoorankings %>% filter(pos != "DEF", pos != "K")
+    yahoorankings <- yahoorankings %>% select(-pos)
 
-  cn <- DBI::dbConnect(RSQLite::SQLite(), "/Users/colin/Documents/GitHub/FantasyFootballData/data/YahooRankings.sqlite3")
+    yahoorankings$rank <- c(1:nrow(yahoorankings))
 
-  t <- tryCatch({DBI::dbGetQuery(cn, "Select * from YahooRankings")},
-                error = function(e) {
-                  if(grepl("no such table", e$message)) {
-                    print("Table not found. Writing to new table.")
-                    DBI::dbWriteTable(cn, "YahooRankings", yahoorankings)
-                  } else {
-                    stop("Error:", e)
-                  }
-                }
-  )
+    colnames(yahoorankings) <- c("Player", "Team", format(Sys.Date(), "%m/%d"))
 
-  if(class(t) == "logical") {
-    t <- DBI::dbGetQuery(cn, "Select * from YahooRankings")
-    for(i in 2:ncol(t)) {
-      t[,i] <- as.numeric(t[,i])
+    ranks <- merge(ranks, yahoorankings, by = c("Player", "Team"))
+
+    nonunique <- grep("X", colnames(ranks))
+    if(length(nonunique) > 0) {
+      ranks <- ranks[,-nonunique]
     }
+
+    ranks <- ranks[order(ranks[,ncol(ranks)]),]
+    ranks$match <- ranks[,ncol(ranks) - 1] == ranks[,ncol(ranks)]
+
+    if(FALSE %in% ranks$match & ncol(ranks) > 4) {
+      write.csv(ranks %>% select(-match), file = "/Users/colin/Documents/GitHub/FantasyFootballData/data/yahoo_ranks.csv", row.names = F)
+    }
+    ranks$match <- NULL
   }
 
-  t <- merge(t, yahoorankings, by = c("Player", "Team"))
-  t <- t %>% select(Player, Team, colnames(t)[c(3:(ncol(t) - 0))])
-  t <- t[order(t[,ncol(t)]),]
-
-  maxdate <- colnames(t)[ncol(t)]
-  if(Sys.Date() >= as.Date(paste0("2018/", maxdate), "%Y/%m/%d")) {
-
-    t$match <- t[,ncol(t) - 1] == t[,ncol(t)]
-    if(FALSE %in% t$match) {
-      DBI::dbSendQuery(cn, "Drop Table YahooRankings")
-      DBI::dbWriteTable(cn, "YahooRankings", t %>% select(-match))
-    }
-    t$match <- NULL
-
-  }
-
-
-  return(t)
+  return(ranks)
 }
